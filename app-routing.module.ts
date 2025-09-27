e19d783ccc560788ad2482ae21e56c62c8823620
João Pedro Quadro Carvalho <jpgnoatto15@gmail.com>
2025-09-27 11:25:47 -0300

    Create app-routing.module.ts

---
+++ app-routing.module.ts
@@ -0,0 +1,8 @@
+import { CambiosComponent } from './cambios/cambios.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+
+const routes: Routes = [
+  // ... outras rotas
+  { path: 'cambios', component: CambiosComponent },
+  { path: 'extrato', component: ExtratoComponent }
+];
