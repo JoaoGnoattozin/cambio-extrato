5e076b69140b2b3e719112355f1399b9dd757628
João Pedro Quadro Carvalho <jpgnoatto15@gmail.com>
2025-09-29 13:11:37 -0300

    Update environment.ts

--- environment.ts
+++ environment.ts
@@ -1,7 +1,7 @@
 // environment.ts (substitua os valores pelas URLs reais da sua API)
 export const environment = {
   production: false,
-  apiBase: 'https://api.exemplo.com', // <-- COLE AQUI A BASE DA SUA API (ex: https://backend.seu-dominio.com)
+  apiBase: 'https://api.exemplo.com', // <-- COLE AQUI A BASE API (ex: https://backend.seu-dominio.com)
   exchangePath: '/exchange',          // caminho opcional para câmbio (ajuste se necessário)
   transactionsPath: '/transactions'   // caminho opcional para extrato (ajuste se necessário)
 };
